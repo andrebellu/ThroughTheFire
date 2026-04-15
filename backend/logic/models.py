ddd0f0b7b559da576c999d83211743329eef01cf
andre <bellu.andrea@outlook.it>
2026-04-15 20:51:59 +0200

    feat: added State class attributes and comparison methods

--- backend/logic/models.py
+++ backend/logic/models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import FrozenSet
 
 class Type(Enum):
     WALL = 0
@@ -19,5 +20,19 @@ class Position:
 
 @dataclass(frozen=True)
 class State:
-    pass
+    robot_position: Position
+    battery: int
+    saved_people: FrozenSet[int]
+    extinguished_fires: FrozenSet[Position]
 
+    g: int = 0
+    h: int = 0
+
+    @property
+    def f(self) -> int:
+        return self.g + self.h
+
+    def __lt__(self, other):
+        if self.f == other.f:
+            return self.g > other.g
+        return self.f < other.f
